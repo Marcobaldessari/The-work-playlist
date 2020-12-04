7e9c8cfd735d002dc8fe838dcf7cc2efa121302f
Marco Baldessari <marco.baldes@gmail.com>
2020-12-05 00:49:45 +0100

    music player fixed, added spotify cta

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var playing = false
+var track,
+    trackDelay,
+    playing,
+    volumeNumber,
+    trackNumber,
+    volumeSize = 3
 
 var crackle = new Howl({
     src: ['tracks/crackle.mp3'],
@@ -7,33 +12,63 @@ var crackle = new Howl({
     volume: .5,
 });
 
+var Albums = document.getElementsByClassName("album");
+for (var i = 0; i < Albums.length; i++) {
+    Albums.item(i).addEventListener("click", playPause);
+}
 
-var sound = new Howl({
-    src: ['tracks/song.mp3'],
-    autoplay: false,
-    loop: false,
-    volume: .2,
-    onend: function () {
-        console.log('Finished!');
-    }
-});
-
-
-var playButton = document.getElementById("play")
-playButton.addEventListener("click", playPause);
 function playPause() {
-    this.classList.toggle("play");
+
+    this.classList.toggle("play")
     if (!playing) {
-        // sound.fade(0.0, 0.7, 5000)
-        // sound.volume(.7)
-        crackle.play()
-        sound.play()
-        playing = true
+        playMusic(this)
+
     } else if (playing) {
-        crackle.stop()
-        sound.stop()
-        playing = false
+        if (volumeNumber == this.getAttribute('volume')) {  // if user is interacting with the same album
+            stopMusic()
+        } else {                // if user is playing another album
+            stopMusic()
+            Albums.item(volumeNumber - 1).classList.toggle("play")
+            playMusic(this)
+        }
     }
 }
 
+function stopMusic() {
+    clearTimeout(trackDelay)
+    crackle.stop()
+    track.stop()
+    playing = false
+}
 
+function playMusic(album) {
+    volumeNumber = album.getAttribute('volume')
+    trackNumber = getRandomInt(volumeSize)
+    track = new Howl({
+        src: ['tracks/volume' + volumeNumber + '/' + trackNumber + '.mp3'],
+        autoplay: false,
+        loop: false,
+        volume: .2,
+        onend: function () {
+            console.log('Finished!');
+        }
+    });
+
+    crackle.volume(1)
+    crackle.play()
+    trackDelay = setTimeout(function () {       // start the track 3s after crackle sound
+        track.play()
+    }, 3000);
+    playing = true
+}
+
+function getRandomInt(max) {        // random int between 1 and max included
+    return Math.floor(Math.random() * Math.floor(max) + 1);
+}
+
+// Enable locomotive scroll
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll]'),
+    smooth: true,
+    direction: "horizontal"
+});
