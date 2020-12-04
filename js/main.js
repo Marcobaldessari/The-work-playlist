f7a5ee81769b1fa0547d92b4a23715cbec34fbfa
Marco Baldessari <marco.baldes@gmail.com>
2020-12-04 12:32:59 +0100

    Added basic player with crackle

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 var playing = false
 
+var crackle = new Howl({
+    src: ['tracks/crackle.mp3'],
+    autoplay: false,
+    loop: false,
+    volume: .5,
+});
+
+
 var sound = new Howl({
     src: ['tracks/song.mp3'],
     autoplay: false,
@@ -14,12 +22,15 @@ var sound = new Howl({
 var playButton = document.getElementById("play")
 playButton.addEventListener("click", playPause);
 function playPause() {
+    this.classList.toggle("play");
     if (!playing) {
         // sound.fade(0.0, 0.7, 5000)
         // sound.volume(.7)
+        crackle.play()
         sound.play()
         playing = true
     } else if (playing) {
+        crackle.stop()
         sound.stop()
         playing = false
     }
