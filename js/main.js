2157f887cb711f0141cb48b782fe0072543de66b
Marco Baldessari <marco.baldes@gmail.com>
2020-12-20 17:59:21 +0100

    added "get in touch" modal

--- js/main.js
+++ js/main.js
@@ -258,6 +258,38 @@ function eventOpenSpotify() {
 }
 
 // --------------------------------
+// Modal
+// --------------------------------
+
+// Get the modal
+var modal = document.getElementById("modalMore");
+
+// Get the button that opens the modal
+var btn = document.getElementById("learnmore");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+// --------------------------------
 // MOBILE KEYBOARD DETECTOR
 // --------------------------------
 
